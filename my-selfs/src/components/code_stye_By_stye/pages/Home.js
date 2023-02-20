3b725556d2b401eaa610f010ef18b0584dd7651d
ismail <ismailsardar@gmail.com>
2023-02-20 22:28:07 +0600

    conditions rendering with useNavigate()

--- my-selfs/src/components/code_stye_By_stye/pages/Home.js
+++ my-selfs/src/components/code_stye_By_stye/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  const handelClick = (url)=>{
+    navigate(url)
+  }
+
   return (
     <div>
       <h1>Home</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore inventore ad fugiat corporis quis omnis quam iure laborum at dicta saepe, eum quaerat quos nulla eaque eligendi rerum dolorem illo.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore inventore ad fugiat corporis quis omnis quam iure laborum at dicta saepe, eum quaerat quos nulla eaque eligendi rerum dolorem illo.</p><br />
+      <button onClick={()=>handelClick('/filter')}>Go filter page</button>
+      <button onClick={()=>handelClick('/about')}>Go about page</button>
     </div>
   )
 }
