85dac82eb47729d4ade02f056143291356d06b98
ismail <ismailsardar@gmail.com>
2023-02-20 11:50:33 +0600

    react Router Link and 404 page page add

--- my-selfs/src/components/code_stye_By_stye/routes/DemoRouts.js
+++ my-selfs/src/components/code_stye_By_stye/routes/DemoRouts.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import About from '../pages/About'
 import Home from '../pages/Home'
+import Page404 from '../pages/Page404'
+import NavDemo from './nav/NavDemo'
 
 const DemoRouts = () => {
   return (
     <div>
         <BrowserRouter>
+        <NavDemo />
          <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            
+            <Route path='/*' element={<Page404 />} />
+            <Route path='/*' element={<Navigate to="/" />} />
          </Routes>
         </BrowserRouter>
     </div>
