b50997b589f58964d52cd857787df575534e83ce
ismail <ismailsardar@gmail.com>
2023-02-21 09:23:59 +0600

    useLocation Hooks by react-router-dom

--- my-selfs/src/components/code_stye_By_stye/nestedRouting/Page1.js
+++ my-selfs/src/components/code_stye_By_stye/nestedRouting/Page1.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Page1 = () => {
+  const location = useLocation();
+  console.log(location);
+  
   return (
     <div>
         <h2>Company</h2>
--- my-selfs/src/components/code_stye_By_stye/pages/Contact.js
+++ my-selfs/src/components/code_stye_By_stye/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <h1>Contact Page</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, laborum voluptatem. Dignissimos, ducimus pariatur impedit iste repellat quos dolor dolore?</p>
         <br /><br /><br />
-        <NavLink to="company" >Company</NavLink>
+        <NavLink to="company" state={{country:"BD",Drictik:"Ku"}} >Company</NavLink>
         <NavLink to="service" >Service</NavLink>
         <NavLink to="project" >Project</NavLink>
         <Outlet />
